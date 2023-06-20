b9c32391b58bbda739a28e9084143553db808bed
RafaTeron <rafael_sabreu@hotmail.com>
2023-06-19 22:14:25 -0300

    Entidades

---
+++ src/main/java/com/rafaTeron/TDDTreine/TddTreineApplication.java
@@ -0,0 +1,13 @@
+package com.rafaTeron.TDDTreine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTreineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddTreineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaTeron/TDDTreine/TddTreineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaTeron.TDDTreine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTreineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
